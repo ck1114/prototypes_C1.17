d8df77c495952dd780b08132d7fcac7f3624fa3a
Charles Kim <Charles Kim>
2017-01-19 16:33:45 -0800

    js event delegation - Charles Kim

--- js_event_delegation/script.js
+++ js_event_delegation/script.js
@@ -1,10 +1,25 @@
+$(document).ready(function () {
+    $("#list").on("click", "button", function(){
+        console.log($(this).text());
+    })
+    var li = $("<li>");
+    var button = $("<button>",{
+        style: "margin: 10px",
+        text: "Delegated Button #5",
+    });
+    $(li).append(button);
+    $("#list").append(li);
+})
+
+
 // Answer the questions here:
 
 // - What do you think is going to happen?
-
+// It will display the text "Button #5"
 // - What happened?
-
+// The text wasn't displayed
 // - Why?
+// I don't know!!!!
 
 //========== Write your code below ===========//
 
