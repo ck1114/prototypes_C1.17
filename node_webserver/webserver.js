9929ea792321e9612f401dab01df9a19ec07736e
Charles Kim <Charles Kim>
2017-02-20 12:17:34 -0800

    v0.1

--- node_webserver/webserver.js
+++ node_webserver/webserver.js
@@ -1,1 +1,12 @@
+var http = require("http");
+var server = http.createServer(handleRequest);
 
+function handleRequest(request, response){
+    console.log("handling request");
+    response.write("initializing request response\n");
+    response.end("It works!! Path Hit: " + request.url);
+}
+
+server.listen(8888, function(){
+    console.log("server listening to port http://localhost:8888");
+});
